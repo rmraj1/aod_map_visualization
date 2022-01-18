dbbd7ae0afb6e86d211d3ffd8bd8eb19d76efd0b
rmraj1 <97936408+rmraj1@users.noreply.github.com>
2022-01-19 02:56:59 +0530

    The python program to visualize AOD datasets

---
+++ codes/aod_plot_ul_anom_v1.py
@@ -0,0 +1,204 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 16 23:29:27 2022
+
+@author: rmraj
+"""
+
+import os
+os.environ['PROJ_LIB'] = r'C:\Users\rmraj\anaconda3\envs\out1\Lib\site-packages\osgeo\data\proj'
+os.environ['GDAL_DATA'] = r'C:\Users\rmraj\anaconda3\envs\out1\Lib\site-packages\osgeo'
+
+import netCDF4
+import xarray as xr
+import numpy as np
+import cmocean # for perceptually uniform colormaps
+import cartopy as cr # for geographic mapping
+import cartopy.crs as ccrs # for map projections
+import matplotlib.pyplot as plt # plotting tool
+import cartopy.feature as cfeature # to add coastlines, land and ocean
+from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
+
+from netCDF4 import Dataset
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import mpl_toolkits.basemap
+from mpl_toolkits.basemap import Basemap
+import pylab as pl
+import sys
+import os
+import matplotlib.colors as colors
+from osgeo import gdal,osr,ogr
+
+import geopandas as gpd
+import fiona
+
+
+
+
+
+# read the netcdf data file
+data1= netCDF4.Dataset(r'D:\extra\assgnmnet\data3\dout1\aodavg_ul1_2018.nc','r')
+data2= netCDF4.Dataset(r'D:\extra\assgnmnet\data3\dout1\aodavg_ul1_2019.nc','r')
+data3= netCDF4.Dataset(r'D:\extra\assgnmnet\data3\dout1\aodavg_ul1_2020.nc','r')
+data4= netCDF4.Dataset(r'D:\extra\assgnmnet\data3\dout1\anom_aod_ul1_2018.nc','r')
+data5= netCDF4.Dataset(r'D:\extra\assgnmnet\data3\dout1\anom_aod_ul1_2019.nc','r')
+
+in1= data1.variables['MYD08_D3_6_1_Deep_Blue_Aerosol_Optical_Depth_550_Land_Mean'][::-1]
+in2= data2.variables['MYD08_D3_6_1_Deep_Blue_Aerosol_Optical_Depth_550_Land_Mean'][::-1]
+in3= data3.variables['MYD08_D3_6_1_Deep_Blue_Aerosol_Optical_Depth_550_Land_Mean'][::-1]
+in4= data4.variables['ANOM_2018_UL1'][::-1]
+in5= data5.variables['ANOM_2019_UL1'][::-1]
+
+# function to create the mask of your shapefile
+def makeMask(lon,lat,res):
+    source_ds = ogr.Open(shapefile)
+    source_layer = source_ds.GetLayer()
+ 
+    # Create high res raster in memory
+    mem_ds = gdal.GetDriverByName('MEM').Create('', lon.size, lat.size, gdal.GDT_Byte)
+    mem_ds.SetGeoTransform((lon.min(), res, 0, lat.max(), 0, -res))
+    band = mem_ds.GetRasterBand(1)
+ 
+    # Rasterize shapefile to grid
+    gdal.RasterizeLayer(mem_ds, [1], source_layer, burn_values=[1])
+ 
+    # Get rasterized shapefile as numpy array
+    array = band.ReadAsArray()
+ 
+    # Flush memory file
+    mem_ds = None
+    band = None
+    return array
+ 
+lats=data1.variables['lat'][:]
+lons=data1.variables['lon'][:]   
+# calculate the cellsize
+cellsize = lons[:][1] - lons[:][0] 
+shapefile = r"D:\extra\assgnmnet\data3\shp\merge2_india.shp"
+
+
+mask = makeMask(lons,lats,cellsize)
+  
+ # show the mask
+plt.imshow(mask)
+plt.show()
+
+var1_1 = np.ma.masked_where(mask==0,in1)
+var1=var1_1[::-1]
+var2_1 = np.ma.masked_where(mask==0,in2)
+var2=var2_1[::-1]
+var3_1 = np.ma.masked_where(mask==0,in3)
+var3=var3_1[::-1]
+var4_1 = np.ma.masked_where(mask==0,in4)
+var4=var4_1[::-1]
+var5_1 = np.ma.masked_where(mask==0,in5)
+var5=var5_1[::-1]
+
+   
+mp=Basemap(projection ="merc",
+           llcrnrlon=60,
+           llcrnrlat=5,
+           urcrnrlon=100,
+           urcrnrlat=40,
+           resolution = 'i')
+
+
+plt.figure(figsize=(12,8))
+
+plt.subplot(2,3,1)
+
+lon,lat=np.meshgrid(lons,lats)
+x,y=mp(lon,lat)
+c=mp.pcolormesh(x,y,var1,vmin=0, vmax=1,cmap='jet')
+
+
+parallels = np.arange(0.,45.,5.)
+mp.drawparallels(parallels,labels=[1,0,0,1],linewidth=0.0)
+meridians = np.arange(60.,100.,10.)
+mp.drawmeridians(meridians,labels=[1,0,0,1],linewidth=0.0)
+mp.readshapefile(r"D:\extra\assgnmnet\data1\modisdata\out\India_State_Boundary_Updated", name="India_State_Boundary_Updated")
+plt.title("2018 UL1")
+
+
+plt.subplot(2,3,2)
+lon,lat=np.meshgrid(lons,lats)
+x,y=mp(lon,lat)
+
+c=mp.pcolormesh(x,y,var2,vmin=0, vmax=1,cmap='jet')
+
+
+parallels = np.arange(0.,45.,5.)
+mp.drawparallels(parallels,labels=[1,0,0,1],linewidth=0.0)
+meridians = np.arange(60.,100.,10.)
+mp.drawmeridians(meridians,labels=[1,0,0,1],linewidth=0.0)
+
+mp.readshapefile(r"D:\extra\assgnmnet\data1\modisdata\out\India_State_Boundary_Updated", name="India_State_Boundary_Updated")
+plt.title("2019 UL1")
+
+plt.subplot(2,3,3)
+lon,lat=np.meshgrid(lons,lats)
+x,y=mp(lon,lat)
+
+c=mp.pcolormesh(x,y,var3,vmin=0, vmax=1,cmap='jet')
+
+
+
+
+
+
+parallels = np.arange(0.,45.,5.)
+mp.drawparallels(parallels,labels=[1,0,0,1],linewidth=0.0)
+meridians = np.arange(60.,100.,10.)
+mp.drawmeridians(meridians,labels=[1,0,0,1],linewidth=0.0)
+mp.readshapefile(r"D:\extra\assgnmnet\data1\modisdata\out\India_State_Boundary_Updated", name="India_State_Boundary_Updated")
+plt.title("2020 UL1")
+
+
+plt.subplot(2,3,4)
+
+
+lon,lat=np.meshgrid(lons,lats)
+x,y=mp(lon,lat)
+c=mp.pcolormesh(x,y,var4,vmin=-0.7, vmax=0.7,cmap='seismic')
+
+
+parallels = np.arange(0.,45.,5.)
+mp.drawparallels(parallels,labels=[1,0,0,1],linewidth=0.0)
+meridians = np.arange(60.,100.,10.)
+mp.drawmeridians(meridians,labels=[1,0,0,1],linewidth=0.0)
+mp.readshapefile(r"D:\extra\assgnmnet\data1\modisdata\out\India_State_Boundary_Updated", name="India_State_Boundary_Updated")
+plt.title("AOD Anomaly 2018-2020 UL1")
+
+plt.subplot(2,3,5)
+lon,lat=np.meshgrid(lons,lats)
+x,y=mp(lon,lat)
+
+
+c=mp.pcolormesh(x,y,var5,vmin=-0.7, vmax=0.7,cmap='seismic')
+
+parallels = np.arange(0.,45.,5.)
+mp.drawparallels(parallels,labels=[1,0,0,1],linewidth=0.0)
+meridians = np.arange(60.,100.,10.)
+mp.drawmeridians(meridians,labels=[1,0,0,1],linewidth=0.0)
+
+mp.readshapefile(r"D:\extra\assgnmnet\data1\modisdata\out\India_State_Boundary_Updated", name="India_State_Boundary_Updated")
+plt.title("AOD Anomaly 2019-2020 UL1")
+
+
+
+plt.savefig(r"D:\extra\assgnmnet\presentatiom\final_figures\anom_ul1_aod.png",
+            bbox_inches ="tight",
+            pad_inches = 1,
+            transparent = True,
+            facecolor ="w",
+            edgecolor ='w',
+            orientation ='landscape')
+
+
+plt.show()
+
+
+
+ 
---
+++ codes/aod_spatial_avg_2018_v1.py
@@ -0,0 +1,223 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 17 00:45:01 2022
+
+@author: rmraj
+"""
+
+import netCDF4
+from netCDF4 import Dataset
+import xarray as xr
+import numpy as np
+import cmocean # for perceptually uniform colormaps
+import cartopy as cr # for geographic mapping
+import cartopy.crs as ccrs # for map projections
+import matplotlib.pyplot as plt # plotting tool
+import cartopy.feature as cfeature # to add coastlines, land and ocean
+from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
+
+
+
+data1= netCDF4.Dataset(r'D:\extra\assgnmnet\data3\dout1\aod_avg_pl_2003_20.nc','r')
+# read and combine many files along a dimension
+ds_comb1 = xr.open_mfdataset(r'D:\extra\assgnmnet\data3\dout1\anom_aod_pl1_2018.nc',combine='by_coords')
+ds_comb2 = xr.open_mfdataset(r'D:\extra\assgnmnet\data3\dout1\anom_aod_ld1_2018.nc',combine='by_coords')
+ds_comb3= xr.open_mfdataset(r'D:\extra\assgnmnet\data3\dout1\anom_aod_ld2_2018.nc',combine='by_coords')
+ds_comb4 = xr.open_mfdataset(r'D:\extra\assgnmnet\data3\dout1\anom_aod_ld3_2018.nc',combine='by_coords')
+ds_comb5 = xr.open_mfdataset(r'D:\extra\assgnmnet\data3\dout1\anom_aod_ld4_2018.nc',combine='by_coords')
+ds_comb6 = xr.open_mfdataset(r'D:\extra\assgnmnet\data3\dout1\anom_aod_ul1_2018.nc',combine='by_coords')
+
+lats=data1.variables['lat'][:]
+lons=data1.variables['lon'][:]
+
+y1=ds_comb1.ANOM_2018_PL
+y2=ds_comb2.ANOM_2018_LD1
+y3=ds_comb3.ANOM_2018_LD2
+y4=ds_comb4.ANOM_2018_LD3
+y5=ds_comb5.ANOM_2018_LD4
+y6=ds_comb6.ANOM_2018_UL1
+
+delhi_d_18_pl1=y1.sel(LAT=slice(17,18), LON=slice(76,77))
+delhi_pl1=np.nanmean(delhi_d_18_pl1)
+
+delhi_d_18_ld1=y2.sel(LAT=slice(17,18), LON=slice(76,77))
+delhi_ld1=np.nanmean(delhi_d_18_ld1)
+
+delhi_d_18_ld2=y3.sel(LAT=slice(17,18), LON=slice(76,77))
+delhi_ld2=np.nanmean(delhi_d_18_ld2)
+
+delhi_d_18_ld3=y4.sel(LAT=slice(17,18), LON=slice(76,77))
+delhi_ld3=np.nanmean(delhi_d_18_ld3)
+
+delhi_d_18_ld4=y5.sel(LAT=slice(17,18), LON=slice(76,77))
+delhi_ld4=np.nanmean(delhi_d_18_ld4)
+delhi_d_18_ul1=y6.sel(LAT=slice(17,18), LON=slice(76,77))
+delhi_ul1=np.nanmean(delhi_d_18_ul1)
+
+
+ci_d_18_pl1=y1.sel(LAT=slice(16,22), LON=slice(73,84))
+ci_pl1=np.nanmean(ci_d_18_pl1)
+
+ci_d_18_ld1=y2.sel(LAT=slice(16,22), LON=slice(73,84))
+ci_ld1=np.nanmean(ci_d_18_ld1)
+
+ci_d_18_ld2=y3.sel(LAT=slice(16,22), LON=slice(73,84))
+ci_ld2=np.nanmean(ci_d_18_ld2)
+
+ci_d_18_ld3=y4.sel(LAT=slice(16,22), LON=slice(73,84))
+ci_ld3=np.nanmean(ci_d_18_ld3)
+
+ci_d_18_ld4=y5.sel(LAT=slice(16,22), LON=slice(73,84))
+ci_ld4=np.nanmean(ci_d_18_ld4)
+
+ci_d_18_ul1=y6.sel(LAT=slice(16,22), LON=slice(73,84))
+ci_ul1=np.nanmean(ci_d_18_ul1)
+
+
+ig_d_18_pl1=y1.sel(LAT=slice(21,25), LON=slice(76,88))
+ig_pl1=np.nanmean(ig_d_18_pl1)
+
+ig_d_18_ld1=y2.sel(LAT=slice(21,25), LON=slice(76,88))
+ig_ld1=np.nanmean(ig_d_18_ld1)
+
+ig_d_18_ld2=y3.sel(LAT=slice(21,25), LON=slice(76,88))
+ig_ld2=np.nanmean(ig_d_18_ld2)
+
+ig_d_18_ld3=y4.sel(LAT=slice(21,25), LON=slice(76,88))
+ig_ld3=np.nanmean(ig_d_18_ld3)
+
+ig_d_18_ld4=y5.sel(LAT=slice(21,25), LON=slice(76,88))
+ig_ld4=np.nanmean(ig_d_18_ld4)
+
+ig_d_18_ul1=y6.sel(LAT=slice(21,25), LON=slice(76,88))
+ig_ul1=np.nanmean(ig_d_18_ul1)
+
+
+
+ne_d_18_pl1=y1.sel(LAT=slice(22,30), LON=slice(89,97))
+ne_pl1=np.nanmean(ne_d_18_pl1)
+
+ne_d_18_ld1=y2.sel(LAT=slice(22,30), LON=slice(89,97))
+ne_ld1=np.nanmean(ne_d_18_ld1)
+
+ne_d_18_ld2=y3.sel(LAT=slice(22,30), LON=slice(89,97))
+ne_ld2=np.nanmean(ne_d_18_ld2)
+
+ne_d_18_ld3=y4.sel(LAT=slice(22,30), LON=slice(89,97))
+ne_ld3=np.nanmean(ne_d_18_ld3)
+
+ne_d_18_ld4=y5.sel(LAT=slice(22,30), LON=slice(89,97))
+ne_ld4=np.nanmean(ne_d_18_ld4)
+
+ne_d_18_ul1=y6.sel(LAT=slice(22,30), LON=slice(89,97))
+ne_ul1=np.nanmean
+
+
+sp_d_18_pl1=y1.sel(LAT=slice(7,14), LON=slice(74,83))
+sp_pl1=np.nanmean(sp_d_18_pl1)
+
+sp_d_18_ld1=y2.sel(LAT=slice(7,14), LON=slice(74,83))
+sp_ld1=np.nanmean(sp_d_18_ld1)
+
+sp_d_18_ld2=y3.sel(LAT=slice(7,14), LON=slice(74,83))
+sp_ld2=np.nanmean(sp_d_18_ld2)
+
+sp_d_18_ld3=y4.sel(LAT=slice(7,14), LON=slice(74,83))
+sp_ld3=np.nanmean(sp_d_18_ld3)
+
+sp_d_18_ld4=y5.sel(LAT=slice(7,14), LON=slice(74,83))
+sp_ld4=np.nanmean(sp_d_18_ld4)
+
+sp_d_18_ul1=y6.sel(LAT=slice(7,14), LON=slice(74,83))
+sp_ul1=np.nanmean(sp_d_18_ul1)
+
+pl_sp="spone" + str(sp_pl1)
+print(pl_sp)
+ld1_sp="two" + str(sp_ld1)
+print(ld1_sp)
+ld2_sp="three" + str(sp_ld2)
+print(ld2_sp)
+ld3_sp="four" + str(sp_ld3)
+print(ld3_sp)
+
+ld4_sp="five" + str(sp_ld4)
+print(ld4_sp)
+
+
+ul1_sp="six" + str(sp_ul1)
+print(ul1_sp)
+
+
+
+
+pl_ig="igone" + str(ig_pl1)
+print(pl_ig)
+ld1_ig="two" + str(ig_ld1)
+print(ld1_ig)
+ld2_ig="three" + str(ig_ld2)
+print(ld2_ig)
+ld3_ig="four" + str(ig_ld3)
+print(ld3_ig)
+
+ld4_ig="five" + str(ig_ld4)
+print(ld4_ig)
+
+
+ul1_ig="six" + str(ig_ul1)
+print(ul1_ig)
+
+pl_ne="neone" + str(ne_pl1)
+print(pl_ne)
+ld1_ne="two" + str(ne_ld1)
+print(ld1_ne)
+ld2_ne="three" + str(ne_ld2)
+print(ld2_ne)
+ld3_ne="four" + str(ne_ld3)
+print(ld3_ne)
+
+ld4_ne="five" + str(ne_ld4)
+print(ld4_ne)
+
+
+ul1_ne="six" + str(ne_ul1)
+print(ul1_ne)
+
+
+
+pl_ci="cione" + str(ci_pl1)
+print(pl_ci)
+ld1_ci="two" + str(ci_ld1)
+print(ld1_ci)
+ld2_ci="three" + str(ci_ld2)
+print(ld2_ci)
+ld3_ci="four" + str(ci_ld3)
+print(ld3_ci)
+
+ld4_ci="five" + str(ci_ld4)
+print(ld4_ci)
+
+
+ul1_ci="six" + str(ci_ul1)
+print(ul1_ci)
+
+
+
+pl_delhi="odelhi" + str(delhi_pl1)
+print(pl_delhi)
+ld1_delhi="two" + str(delhi_ld1)
+print(ld1_delhi)
+ld2_delhi="three" + str(delhi_ld2)
+print(ld2_delhi)
+ld3_delhi="four" + str(delhi_ld3)
+print(ld3_delhi)
+
+ld4_delhi="five" + str(delhi_ld4)
+print(ld4_delhi)
+
+
+ul1_delhi="six" + str(delhi_ul1)
+print(ul1_delhi)
+
+
+
+
